d861def665000fc36e70949872ac2df6cb4169f5
connorwaslo <connorwaslo@gmail.com>
2020-03-28 21:01:52 -0700

    Pull contacts

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Friends from './src/views/Friends';
 
 const Stack = createStackNavigator();
--- src/views/Friends.js
+++ src/views/Friends.js
@@ -1,7 +1,36 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 function Friends() {
+    let contacts = [];
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Contacts.requestPermissionsAsync();
+            if (status === 'granted') {
+                const { data } = await Contacts.getContactsAsync({
+                    fields: [Contacts.Fields.PhoneNumbers]
+                });
+
+                data.forEach(contact => {
+                    if (!contact.phoneNumbers) return;
+                    console.log('Adding', contact.name);
+
+                    let newContact = {
+                        name: contact.name,
+                        phoneNumbers: contact.phoneNumbers.map(num => {
+                            return num.digits;
+                        })
+                    };
+
+                    // Push new contact to state
+                    contacts.push(newContact);
+                });
+            }
+        })();
+    }, []);
+
     return (
         <View>
             <Text>Friends</Text>
