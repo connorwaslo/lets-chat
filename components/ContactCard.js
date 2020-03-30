194b79abd3d554cb4c0d42c45978f345b4549bb4
connorwaslo <connorwaslo@gmail.com>
2020-03-29 20:19:15 -0700

    Request friends
    Adds phone number to firebase

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -1,9 +1,57 @@
 import React from 'react';
 import { TouchableOpacity, StyleSheet, View, Text } from 'react-native';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 function ContactCard({ userInfo }) {
     const { name } = userInfo;
     const phone = userInfo.phoneNumbers[0]; // Todo: Parse this
+
+    /*
+     * This function is responsible for:
+     * - Adding contact to database if they don't exist already
+     * - Prepping a "share" text message to individuals who don't have accounts (if user accepts)
+     */
+    function _handleInviteFriend(reqPhone) {
+        // Check if already sent request
+        // Todo: Phone number replaced by current user phone number
+        firebase.database().ref('+16025554181/friendRequests/').once('value')
+            .then(snapshot => {
+                if (snapshot.val()) {
+                    // console.log('No friend request sent yet');
+                    let pendingReqs = [];
+                    for (const index in snapshot.val()) {
+                        console.log(snapshot.val()[index]);
+                        pendingReqs.push(snapshot.val()[index]);
+                    }
+
+                    // Todo: store this in redux so we're not constantly doing firebase reads
+
+                    console.log('Pending friend requests:', pendingReqs);
+
+                    if (!pendingReqs.includes(phone)) {
+                        _requestFriend(pendingReqs);
+                    }
+                }
+            })
+            .catch(error => {
+                console.log('Something went wrong checking if friend request already exists:', error.message);
+            });
+    }
+
+    /*
+     * To be called once firebase already checked for duplicates
+     */
+    function _requestFriend(pendingReqs) {
+        firebase.database().ref('+16025554181/friendRequests')
+            .set([
+                ...pendingReqs,
+                phone
+            ])
+            .catch(error => {
+                console.log('Something went wrong requesting friend', error.message);
+            })
+    }
 
     return (
         <View style={styles.container}>
@@ -11,7 +59,7 @@ function ContactCard({ userInfo }) {
                 <Text style={styles.contact}>{name}{'\n'}{phone}</Text>
             </View>
             <View style={styles.right}>
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={() => _handleInviteFriend()}>
                     <View style={styles.addButton}>
                         <Text>+</Text>
                     </View>
